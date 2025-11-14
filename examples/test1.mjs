40b4a692c576ee56ba1ce5b613c91b6dede27d5f
heiwa4126 <heiwa4126@gmail.com>
2025-11-14 15:31:11 +0900

    remove link and smoke-test dir

--- examples/test1.mjs
+++ examples/test1.mjs
@@ -1,3 +1,5 @@
+// npm build した後に実行すること
+
 import { hello } from "@heiwa4126/hello4";
 
 console.log(hello()); // "Hello!"
--- examples/test2.cjs
+++ examples/test2.cjs
@@ -1,3 +1,5 @@
+// npm build した後に実行すること
+
 const { hello } = require("@heiwa4126/hello4");
 
 console.log(hello()); // "Hello!"
